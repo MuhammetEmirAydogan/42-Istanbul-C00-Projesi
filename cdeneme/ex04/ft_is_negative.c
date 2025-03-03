e13a12ab90e77f52c594c185d1d3825a28b27de0
MuhammetEmirAydogan <m.emr.aydogan@gmail.com>
2025-03-03 17:23:28 +0300

    ft_is_negative.c

--- cdeneme/ex04/ft_is_negative.c
+++ cdeneme/ex04/ft_is_negative.c
@@ -12,10 +12,14 @@
 
 #include <unistd.h>
 
-void	ft_is_negative(int n)
+void ft_is_negative(int n)
 {
-	if (n >= 0)
-		write(1, "P", 1);
-	else
-		write(1, "N", 1);
+    // Burada 'n' pozitif ya da sıfır mı diye kontrol ediyorum.
+    if (n >= 0)
+        // Eğer 'n' pozitif veya sıfırsa, "P" harfini ekrana yazdırıyorum (pozitif).
+        write(1, "P", 1);
+    else
+        // Eğer 'n' negatifse, "N" harfini ekrana yazdırıyorum (negatif).
+        write(1, "N", 1);
 }
+
