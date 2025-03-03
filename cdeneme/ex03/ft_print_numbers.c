cf5956a2e1919e29518e271f88f12a868dc5f839
MuhammetEmirAydogan <m.emr.aydogan@gmail.com>
2025-03-03 17:22:24 +0300

    ft_print_numbers.c

--- cdeneme/ex03/ft_print_numbers.c
+++ cdeneme/ex03/ft_print_numbers.c
@@ -12,14 +12,21 @@
 
 #include <unistd.h>
 
-void	ft_print_numbers(void)
+void ft_print_numbers(void)
 {
-	int		decimal;
+    // 'decimal' değişkenini '0' rakamının ASCII değeri olan 48 ile başlatıyorum.
+    int decimal = 48;
 
-	decimal = 48;
-	while (decimal < 58)
-	{
-		write(1, &decimal, 1);
-		decimal++;
-	}
+    // Burada 'decimal' değişkenini 57'ye kadar artıracağım, çünkü '9' rakamının ASCII değeri 57'dir.
+    while (decimal < 58)
+    {
+        // 'write' fonksiyonunu kullanarak, 'decimal' değişkeninin ASCII değeri olan karakteri ekrana yazdırıyorum.
+        // 1, çıktının ekrana (standart çıkış) yazdırılacağını belirtir.
+        // &decimal, yazdırılacak olan sayının bellekteki adresini belirtir.
+        write(1, &decimal, 1);
+
+        // 'decimal' değişkenini bir sonraki rakama geçirecek şekilde arttırıyorum.
+        decimal++;
+    }
 }
+
