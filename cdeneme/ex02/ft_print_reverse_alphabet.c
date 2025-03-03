f2463f0274a4ba9c02c9786fee815dc4565ba8a6
MuhammetEmirAydogan <m.emr.aydogan@gmail.com>
2025-03-03 17:21:25 +0300

    ft_print_reverse_alphabet.c

--- cdeneme/ex02/ft_print_reverse_alphabet.c
+++ cdeneme/ex02/ft_print_reverse_alphabet.c
@@ -12,14 +12,21 @@
 
 #include <unistd.h>
 
-void	ft_print_reverse_alphabet(void)
+void ft_print_reverse_alphabet(void)
 {
-	int		letter;
+    // 'letter' değişkenini 'z' harfiyle başlatıyorum.
+    int letter = 'z';
 
-	letter = 'z';
-	while (letter >= 'a')
-	{
-		write(1, &letter, 1);
-		letter--;
-	}
+    // Burada 'letter' değişkeni 'a' harfine kadar dönecek.
+    while (letter >= 'a')
+    {
+        // 'write' fonksiyonu ile 'letter' değişkeninin içeriğini ekrana yazdırıyorum.
+        // 1, çıktının ekrana (standart çıkış) yazdırılacağını belirtir.
+        // &letter, yazdırılacak olan harfin bellekteki adresini belirtir.
+        write(1, &letter, 1);
+
+        // 'letter' değişkenini bir önceki harfe geçirmek için azaltıyorum.
+        letter--;
+    }
 }
+
