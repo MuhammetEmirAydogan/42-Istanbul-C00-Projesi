54223b6b88ecc84f89d9fd6e4ddae635e7e55d9c
MuhammetEmirAydogan <m.emr.aydogan@gmail.com>
2025-03-03 17:19:29 +0300

    ft_print_alphabet.c

--- cdeneme/ex01/ft_print_alphabet.c
+++ cdeneme/ex01/ft_print_alphabet.c
@@ -12,14 +12,20 @@
 
 #include <unistd.h>
 
-void	ft_print_alphabet(void)
+void ft_print_alphabet(void)
 {
-	int		letter;
+    // 'letter' değişkenini 'a' harfiyle başlatıyorum.
+    int letter = 'a';
 
-	letter = 'a';
-	while (letter <= 'z')
-	{
-		write(1, &letter, 1);
-		letter++;
-	}
+    // Burada 'letter' değişkeni 'z' harfine kadar dönecek.
+    while (letter <= 'z')
+    {
+        // 'write' fonksiyonu ile ekrana 'letter' değişkeninin içeriğini yazdırıyorum.
+        // 1, standart çıkışı (ekran) temsil eder, &letter ise 'letter' değişkeninin adresidir.
+        write(1, &letter, 1);
+        
+        // 'letter' değişkenini bir sonraki harfe geçirmek için arttırıyorum.
+        letter++;
+    }
 }
+
